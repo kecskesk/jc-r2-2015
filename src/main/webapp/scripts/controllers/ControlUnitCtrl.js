86095cf8065469fcc7a5bf1a2fdaef29d334dc7d
Kecskés Krisztián <kk1205@sch.bme.hu>
2015-12-03 12:25:11 +0100

    angular design and default paramclass

--- src/main/webapp/scripts/controllers/ControlUnitCtrl.js
+++ src/main/webapp/scripts/controllers/ControlUnitCtrl.js
@@ -12,5 +12,36 @@ app.controller('ControlUnitCtrl', function ($scope, controlUnitService) {
     $scope.stopBinding = controlUnitService.stopBinding;
     
     $scope.selectedDirection = "";
+    $scope.selectedStrategy = "";
     $scope.selectedUnit = 0;
+    $scope.selectedX = 0;
+    $scope.selectedY = 0;
+    
+    $scope.add = false;
+    
+    $scope.setStrategy = function() {
+        if($scope.selectedStrategy === "DEF") {
+            if($scope.add){
+                controlUnitService.addDefaultStrategyOfBuilder($scope.selectedUnit, $scope.selectedDirection);
+            } else {
+                controlUnitService.changeToDefaultStrategyOfBuilder($scope.selectedUnit, $scope.selectedDirection);
+            }
+        }  
+        
+        if($scope.selectedStrategy === "CELL") {
+            
+        }  
+        
+        if($scope.selectedStrategy === "GOTO") {
+            if($scope.add){
+                controlUnitService.addGoToStrategyOfBuilder($scope.selectedUnit, $scope.selectedX, $scope.selectedY);
+            } else {
+                controlUnitService.changeToGoToStrategyOfBuilder($scope.selectedUnit, $scope.selectedX, $scope.selectedY);
+            }
+        }
+    }
+    
+    $scope.changeMode = function() {
+        $scope.add = !$scope.add;
+    }
 });
