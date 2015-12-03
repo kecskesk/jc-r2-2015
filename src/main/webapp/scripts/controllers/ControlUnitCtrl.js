a14f6b704c185fe501de3d29488d7d7c13306882
Kecskés Krisztián <kk1205@sch.bme.hu>
2015-12-03 12:37:08 +0100

    angular0.3

--- src/main/webapp/scripts/controllers/ControlUnitCtrl.js
+++ src/main/webapp/scripts/controllers/ControlUnitCtrl.js
@@ -5,7 +5,7 @@
 app.controller('ControlUnitCtrl', function ($scope, controlUnitService) {
     controlUnitService.startBinding();
 
-    $scope.latestStanding = controlUnitService.getLatestStanding;
+    $scope.latestStanding = function () { return controlUnitService.getLatestStanding(); }
     $scope.startGame = controlUnitService.startGame;
     $scope.stopGame = controlUnitService.stopGame;
     $scope.startBinding = controlUnitService.startBinding;
